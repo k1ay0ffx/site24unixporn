0de6ef4667b2889302861d0f2603bd0bec43db6a
klay <xclay315@gmail.com>
2026-02-23 17:33:04 +0500

    feat: add user email

--- user.service.ts
+++ user.service.ts
@@ -1,6 +1,6 @@
 // src/app/services/user.service.ts
 export class UserService {
   getUser() {
-    return { id: 1, name: 'Admin' };
+    return { id: 1, name: 'Admin', email: 'admin@work.com' };
   }
 }
